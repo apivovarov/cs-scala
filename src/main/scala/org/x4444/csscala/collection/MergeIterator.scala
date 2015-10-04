f7341761900b7744ae5409cd0b5ab0cda59d3115
Alexander Pivovarov <alex.pivovarov@radius.com>
2015-10-03 23:07:34 -0700

    Add MergeIterator

---
+++ src/main/scala/org/x4444/csscala/collection/MergeIterator.scala
@@ -0,0 +1,28 @@
+package org.x4444.csscala.collection
+
+import scala.collection.mutable.SortedSet
+
+class MergeIterator[T: Ordering](iters: Seq[Iterator[T]]) extends Iterator[T] {
+
+  type AB = (T, Iterator[T])
+
+  implicit val abOrdering: Ordering[AB] = Ordering.by(_._1)
+
+  val heads: SortedSet[AB] =
+    iters.flatMap { x => if (x.hasNext) Some((x.next, x)) else None }.foldLeft(SortedSet.empty[AB])(_ += _)
+
+  override def next(): T = {
+    if (heads.isEmpty) {
+      throw new java.util.NoSuchElementException("next on empty iterator")
+    }
+    val minEl = heads.min
+    val (minV, iter) = minEl
+    heads -= minEl
+    if (iter.hasNext) {
+      heads += ((iter.next, iter))
+    }
+    minV
+  }
+
+  override def hasNext(): Boolean = !heads.isEmpty
+}
