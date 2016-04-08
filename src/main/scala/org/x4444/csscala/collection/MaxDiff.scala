d1e321ced26d25d11ea4b04aa5f3fd5898ce0bee
Alexander Pivovarov <alex.pivovarov@radius.com>
2016-04-08 13:37:59 -0700

    Add recursive MaxDiff for array

--- src/main/scala/org/x4444/csscala/collection/MaxDiff.scala
+++ src/main/scala/org/x4444/csscala/collection/MaxDiff.scala
@@ -1,10 +1,18 @@
 package org.x4444.csscala.collection
 
+import scala.annotation.tailrec
+
 /**
   * Calculates max diff for array. diff conditions: i < j and a[i] < a[j]
   */
 object MaxDiff {
 
+  /**
+    * Regular implementation
+    *
+    * @param arr - Int Array
+    * @return - max diff
+    */
   def get(arr: Array[Int]): Int = {
     if (arr == null || arr.length < 2) return 0
 
@@ -22,5 +30,32 @@ object MaxDiff {
       }
     }
     diff
+  }
+
+  /**
+    * Recursive implementation
+    *
+    * @param arr - Int Array
+    * @return - max diff
+    */
+  def getRec(arr: Array[Int]): Int = {
+    if (arr == null || arr.length < 2) return 0
+
+    look(arr.toList, 0, arr(0))
+  }
+
+  @tailrec
+  def look(li: List[Int], diff: Int, min: Int): Int = {
+    li match {
+      case Nil => diff
+      case x :: tail => {
+        if (x < min) look(tail, diff, x)
+        else {
+          val newDiff = x - min
+          val diff2 = if (newDiff > diff) newDiff else diff
+          look(tail, diff2, min)
+        }
+      }
+    }
   }
 }
--- src/test/scala/org/x4444/csscala/collection/MaxDiffSuite.scala
+++ src/test/scala/org/x4444/csscala/collection/MaxDiffSuite.scala
@@ -4,23 +4,20 @@ import org.scalatest.FunSuite
 
 class MaxDiffSuite extends FunSuite {
 
-  test("get") {
-    assert(MaxDiff.get(Array(1, 2, 3)) == 2)
+  test("functions") {
+    val f1 = MaxDiff.get(_)
+    val f2 = MaxDiff.getRec(_)
 
-    assert(MaxDiff.get(Array(1, 2, 3, 0, 3)) == 3)
-
-    assert(MaxDiff.get(Array(1, 2, 3, 0, 1)) == 2)
-
-    assert(MaxDiff.get(Array(1, 3, 0, 1)) == 2)
-
-    assert(MaxDiff.get(Array(1, 3, 10, 4)) == 9)
-    
-    assert(MaxDiff.get(Array(1, 3, 10, 4, 1)) == 9)
-
-    assert(MaxDiff.get(Array(1, 3, 0, 4)) == 4)
-
-    assert(MaxDiff.get(Array(1)) == 0)
-
-    assert(MaxDiff.get(Array()) == 0)
+    List(f1, f2).foreach { f =>
+      assert(f(Array(1, 2, 3)) == 2)
+      assert(f(Array(1, 2, 3, 0, 3)) == 3)
+      assert(f(Array(1, 2, 3, 0, 1)) == 2)
+      assert(f(Array(1, 3, 0, 1)) == 2)
+      assert(f(Array(1, 3, 10, 4)) == 9)
+      assert(f(Array(1, 3, 10, 4, 1)) == 9)
+      assert(f(Array(1, 3, 0, 4)) == 4)
+      assert(f(Array(1)) == 0)
+      assert(f(Array()) == 0)
+    }
   }
 }
