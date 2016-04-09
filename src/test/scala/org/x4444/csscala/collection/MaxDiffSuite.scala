d27f5467427f44a6a2ee1ef778306d97371333cd
Alexander Pivovarov <alex.pivovarov@radius.com>
2016-04-08 19:20:54 -0700

    Optimize MaxDiff test

--- src/test/scala/org/x4444/csscala/collection/MaxDiffSuite.scala
+++ src/test/scala/org/x4444/csscala/collection/MaxDiffSuite.scala
@@ -5,19 +5,21 @@ import org.scalatest.FunSuite
 class MaxDiffSuite extends FunSuite {
 
   test("functions") {
-    val f1 = MaxDiff.get(_)
-    val f2 = MaxDiff.getRec(_)
+    val inOut = List(
+      (Array(1, 2, 3), 2),
+      (Array(1, 2, 3, 0, 3), 3),
+      (Array(1, 2, 3, 0, 1), 2),
+      (Array(1, 3, 0, 1), 2),
+      (Array(1, 3, 10, 4), 9),
+      (Array(1, 3, 10, 4, 1), 9),
+      (Array(1, 3, 0, 4), 4),
+      (Array(1), 0),
+      (Array(1), 0)
+    )
 
-    List(f1, f2).foreach { f =>
-      assert(f(Array(1, 2, 3)) == 2)
-      assert(f(Array(1, 2, 3, 0, 3)) == 3)
-      assert(f(Array(1, 2, 3, 0, 1)) == 2)
-      assert(f(Array(1, 3, 0, 1)) == 2)
-      assert(f(Array(1, 3, 10, 4)) == 9)
-      assert(f(Array(1, 3, 10, 4, 1)) == 9)
-      assert(f(Array(1, 3, 0, 4)) == 4)
-      assert(f(Array(1)) == 0)
-      assert(f(Array()) == 0)
+    inOut.foreach { x =>
+      assert(MaxDiff.get(x._1) == x._2)
+      assert(MaxDiff.getRec(x._1) == x._2)
     }
   }
 }
